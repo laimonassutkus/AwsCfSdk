aca77669b348fc6acc7b27d37b6b477ff0fdb461
laisut <laimonas.sutkus@gmail.com>
2019-08-26 10:51:17 +0300

    Add cleaning command.

--- build.sh
+++ build.sh
@@ -6,10 +6,11 @@ set -e
 
 python3 setup.py bdist_wheel sdist
 
-while getopts 'iu' flag; do
+while getopts 'iuc' flag; do
   case "${flag}" in
     i) python3 -m pip install . ;;
     u) twine upload dist/* ;;
+    c) rm -rf *.egg-info build dist ;;
     *) echo 'Unexpected flag.'
        exit 1 ;;
   esac
