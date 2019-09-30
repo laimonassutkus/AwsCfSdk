e10d25759af18c06b6020d5132e8e75872b3d23d
laisut <laimonas.sutkus@gmail.com>
2019-09-30 20:59:37 +0300

    Delete all idea files.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aws_infrastructure_sdk',
-    version='1.11.1',
+    version='1.11.2',
     license='GNU GENERAL PUBLIC LICENSE Version 3',
     packages=find_packages(),
     description='SDK that helps to build AWS - CloudFormation infrastructure projects.',
